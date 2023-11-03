b3f12a5aa7a70759202a8ec64db9fe656416a3f7
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-11-03 09:38:23 +0530

    managing workout data: and creating classes

--- script.js
+++ script.js
@@ -13,6 +13,49 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 //Geolocation API
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + ' ').slice(-10);
+
+  constructor(coords, distance, duration) {
+    // this.date= ...;
+    // this.id=...;
+    this.coords = coords; //[lat,lng]
+    this.distance = distance; //in km
+    this.duration = duration; //in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const Cycling1 = new Running([39, -12], 27, 95, 523);
+// console.log(run1, Cycling1);
+///////////////////////////////////////////////////////////
+//Architecture class
 class App {
   #map;
   #mapEvent;
